c934c51034e9902b8fc29689d678d9ce14a4a49c
aleksandr Beliun <aleksandr.beliun@gmail.com>
2019-05-16 12:13:47 +0200

    Added regex to check for valid url

--- background/background.js
+++ background/background.js
@@ -4,27 +4,35 @@ function onError(error) {
   console.log(`An error: ${error}`);
 }
 
+function is_url(str) {
+  regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+
+  return regexp.test(str) ? true : false;
+}
+
 function storeAllTabsAsBookmarks() {
   let getItemList = browser.storage.local.get();
   getItemList.then(tabs => {
     for (const tab in tabs) {
       if (tabs.hasOwnProperty(tab)) {
         const element = tabs[tab];
-        var exists = browser.bookmarks.search({ url: element.url });
-        exists.then(bookmarkItems => {
-          if (bookmarkItems.length > 0) {
-            console.log('Item Already Bookmarked');
-          } else {
-            var createBookmark = browser.bookmarks.create({
-              parentId: bookmarkFolderId,
-              title: element.title,
-              url: element.url
-            });
-            createBookmark.then(node => {
-              console.log(node);
-            }, onError);
-          }
-        }, onError);
+        if (is_url(element.url)) {
+          var exists = browser.bookmarks.search({ url: element.url });
+          exists.then(bookmarkItems => {
+            if (bookmarkItems.length > 0) {
+              console.log('Item Already Bookmarked');
+            } else {
+              var createBookmark = browser.bookmarks.create({
+                parentId: bookmarkFolderId,
+                title: element.title,
+                url: element.url
+              });
+              createBookmark.then(node => {
+                console.log(node);
+              }, onError);
+            }
+          }, onError);
+        }
       }
     }
   }, onError);
