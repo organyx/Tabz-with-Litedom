a1355a82cd80e3400a49e8f1007d04043aa90bf3
aleksandr Beliun <aleksandr.beliun@gmail.com>
2019-05-27 20:48:22 +0200

    Displaying tabs with relift

--- popup/tabz-relift.js
+++ popup/tabz-relift.js
@@ -1,8 +1,25 @@
 import reLiftHTML from './relift.js';
 
+function onGotItem(item) {
+  console.log('onGotItem', item);
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
 reLiftHTML({
   el: '#root',
   data: {
-    world: 'World this is me'
+    world: 'World this is me',
+    itemList: []
+  },
+  created() {
+    let getItemList = browser.storage.local.get();
+    getItemList.then(items => {
+      console.log(items);
+      this.data.itemList = Object.values(items);
+      console.log(this.data.itemList);
+    }, onError);
   }
 });
