b886de2d9ae454775a7a69127361540c5079bcea
aleksandr Beliun <aleksandr.beliun@gmail.com>
2019-06-13 12:37:16 +0200

    Fixed timer setup

--- options/options.js
+++ options/options.js
@@ -1,24 +1,43 @@
-function onSetTimer() {
-  console.log('Timer Set');
-}
+import reLiftHTML from '../popup/relift.js';
+
+reLiftHTML({
+  el: '#root',
+  data: {
+    timer: '',
+    displayTimer: []
+  },
+  created() {
+    console.log('Created ', this.data.timer);
+    var storedTimer = browser.storage.sync.get('timer');
+    storedTimer.then(timer => {
+      this.data.timer = timer;
+      this.data.displayTimer = Object.values(timer);
+      var sendMessage = browser.runtime.sendMessage({ timer: timer });
+      sendMessage.then(response => {
+        console.log('BG Response: ', response);
+      }, onError);
+    }, onError);
+  },
+  setTimer(event) {
+    console.log('Timer', this.data.timer);
+    var saveGlobalTimer = browser.storage.sync.set({
+      timer: this.data.timer
+    });
+    saveGlobalTimer.then(() => {
+      this.data.displayTimer = this.data.timer;
+      console.log('Display Timer ', this.data.displayTimer);
+    }, onError);
+  },
+  getTimer(event) {
+    var storedTimer = browser.storage.sync.get('timer');
+    storedTimer.then(timer => {
+      console.log('getTimer ', timer);
+    }, onError);
+  }
+});
 
 function onError(error) {
   console.log(error);
-}
-
-function saveGlobalTimer(e) {
-  if (e.target.id === 'btn-set-global-timer') {
-    var saveGlobalTimer = browser.storage.sync.set({
-      timer: (selectTimer = document.getElementById('timers').value)
-    });
-    saveGlobalTimer.then(onSetTimer, onError);
-    // restoreOptions();
-  }
-}
-
-function test(e) {
-  var selectTimer = document.getElementById('timers').value;
-  console.log(selectTimer);
 }
 
 function restoreOptions() {
@@ -32,4 +51,3 @@ function restoreOptions() {
 }
 
 browser.runtime.onStartup.addListener(restoreOptions);
-document.addEventListener('click', saveGlobalTimer);
